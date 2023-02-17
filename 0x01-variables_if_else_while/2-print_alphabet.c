61ac89aff5ba0d0da2d123a3a4f50a225392f96c
MkhululiSabelo <mkhululi.mkhwanazi21@gmail.com>
2023-02-17 09:49:04 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - returns alphabets
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int ch = 'a';
+while (ch <= 'z');
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
